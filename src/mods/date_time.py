101f0aba073afea33668e549484f971aec0a0ff1
github-test <test@liuguanglin.com>
2018-03-08 23:57:50 +0800

    date_time 2to3

--- src/mods/date_time.py
+++ src/mods/date_time.py
@@ -5,22 +5,22 @@ Created on 2017年3月21日
 '''
 import time,datetime
 
-print 'time()==>',time.time()
-print 'mktime()==>',time.mktime(time.localtime())
+print('time()==>',time.time())
+print('mktime()==>',time.mktime(time.localtime()))
 
-print 'gmtime()==>',time.gmtime()
-print 'localtime()==>',time.localtime()
+print('gmtime()==>',time.gmtime())
+print('localtime()==>',time.localtime())
 
-print 'strptime()==>',time.strptime('2017-3-21','%Y-%m-%d')
-print 'strftime()==>',time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+print('strptime()==>',time.strptime('2017-3-21','%Y-%m-%d'))
+print('strftime()==>',time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
 
-print 'ctime()==>',time.ctime(time.time())
-print 'asctime()==>',time.asctime(time.localtime())
+print('ctime()==>',time.ctime(time.time()))
+print('asctime()==>',time.asctime(time.localtime()))
 
-print datetime.datetime.now()
+print(datetime.datetime.now())
 dt=datetime.datetime(2017,3,23,20,15,18)
-print dt
-print 'fromtimestamp()==>',datetime.datetime.fromtimestamp(time.time())
-print 'utcfromtimestamp()==>',datetime.datetime.utcfromtimestamp(time.time())
-print dt.timetuple()
-print dt.strftime('%Y-%m-%d %H')
+print(dt)
+print('fromtimestamp()==>',datetime.datetime.fromtimestamp(time.time()))
+print('utcfromtimestamp()==>',datetime.datetime.utcfromtimestamp(time.time()))
+print(dt.timetuple())
+print(dt.strftime('%Y-%m-%d %H'))
