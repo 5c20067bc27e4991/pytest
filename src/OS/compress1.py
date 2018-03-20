c79f2fbf9a213367b908485f2058c4b4d811df13
lgl-test <test@liuguanglin.com>
2018-03-20 18:14:41 +0800

    tarfile fun

--- src/OS/compress1.py
+++ src/OS/compress1.py
@@ -4,28 +4,45 @@ import tarfile
 import time
 
 curr_time = time.strftime('%Y%m%d_%H%M%S', time.localtime())
-src_path = 'res'
-dst_cmp_file = curr_time + '.tgz'
+src_outer_dir = 'res'
+src_dirs = ('C', 'D', '123.txt')
+dst_file_name = curr_time + '.tgz'
 curr_path = os.getcwd()
-cmp_file_full = os.path.join(curr_path, dst_cmp_file)
+cmp_file_full = os.path.join(curr_path, dst_file_name)
 
-##不压缩外层目录，只压缩目标文件和目录
-os.chdir(src_path)
+
+def cmp_dir(src_outer_dir, cmp_file_full, *src_dirs):
+    '''
+    不压缩外层目录，只压缩目标文件夹
+    '''
+    os.chdir(src_outer_dir)
+    tar = tarfile.open(cmp_file_full, 'w:gz')
+    # for dirpath, dirname, filename in os.walk('.'):
+    #     for f in filename:
+    #         tar.add(os.path.join(dirpath, f))
+    #         print('add--' + os.path.join(dirpath, f))
+    #     for d in dirname:
+    #         tar.add(os.path.join(dirpath, d))
+    #         print('add--' + os.path.join(dirpath, d))
+    ##效果相同
+    # for i in os.listdir():
+    #     tar.add(i)
+    for i in src_dirs:
+        print(i)
+        tar.add(i)
+    tar.close()
+    os.chdir(curr_path)
+
+
+cmp_dir(src_outer_dir, cmp_file_full, *src_dirs)
+
+# os.mkdir(curr_time)
+# tar = tarfile.open(dst_cmp_file, 'r')
+# for t in tar:
+#     tar.extract(t, curr_time)
+# tar.close()
+os.chdir(src_outer_dir)
 tar = tarfile.open(cmp_file_full, 'w:gz')
-for dirpath, dirname, filename in os.walk('.'):
-    for f in filename:
-        tar.add(os.path.join(dirpath, f))
-        print('add--' + os.path.join(dirpath, f))
-    for d in dirname:
-        tar.add(os.path.join(dirpath, d))
-        print('add--' + os.path.join(dirpath, d))
-##效果相同
-# for i in os.listdir():
-#     tar.add(i)
-tar.close()
-os.chdir(curr_path)
-os.mkdir(curr_time)
-tar = tarfile.open(dst_cmp_file, 'r')
-for t in tar:
-    tar.extract(t, curr_time)
+for i in ('C', 'D', '123.txt'):
+    tar.add(i)
 tar.close()
