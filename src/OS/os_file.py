aadb36e1a143f8b96632f48ea601c295371f4a86
github-test <test@liuguanglin.com>
2018-04-06 00:40:55 +0800

    文件操作

--- src/OS/os_file.py
+++ src/OS/os_file.py
@@ -18,13 +18,13 @@ print('当前路径：' + os.getcwd())
 # os.remove('a.txt')
 # os.unlink('dir/test')   #删除文件
 
-# os.rename('a', 'x')#重命名文件、目录
-# shutil.move('bbb.txt', '123') # 移动或重命名文件/目录，目标路径不能已经存在
-# shutil.copyfile('a.txt', 'dst/d.txt') #仅复制文件
-# shutil.copy('a.txt', 'dst') #复制文件/到目录，若目标文件存在则覆盖
+# os.rename('x', 'A')#重命名文件、目录
+# shutil.move('123', 'A') # 移动或重命名文件/目录，目标路径不能已经存在
+# shutil.copyfile('123.txt', 'd\d.txt') #复制文件，需写明目标文件名，若目标文件存在则覆盖
+# shutil.copy('123.txt', 'd') #可复制文件到目录，若目标文件存在则覆盖
 # shutil.copy2('a.txt', r'dst') #在copy基础上复制最后访问、修改时间
-# shutil.copymode('src', 'dst') #仅复制文件/目录权限
+# shutil.copymode('c', 'd') #仅复制文件/目录权限
 # shutil.copystat('src', 'dst2') #仅复制文件/目录权限、最后访问、修改时间
-# shutil.copytree('dir', r'dir2')  #复制目录,目标路径不能已经存在
-# print os.stat('src')
+# shutil.copytree('a', 'd\ccc')  #复制目录,目标路径不能已经存在
+# print(os.stat('123.txt'))
 # print os.stat('dst2')
