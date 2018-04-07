12917480244147fadde80f0fb81c3f90389498e6
github-test <test@liuguanglin.com>
2018-04-08 00:59:35 +0800

    urllib-

--- src/spider1/base.py
+++ src/spider1/base.py
@@ -11,18 +11,18 @@ UA = {
 keys = raw_input('Search key: ')
 wd = {'wd': keys}
 wd = urllib.urlencode(wd)
-search_url = 's?'+ wd
+search_url = 's?' + wd
 request = urllib2.Request(baseurl + search_url, headers=UA)
 request.add_header('header1', 'hehe')
 reponse = urllib2.urlopen(request)
 print(request.get_full_url())
 
-#get_header()参数首字母必须大写，剩余均小写
+# get_header()参数首字母必须大写，剩余均小写
 print(request.get_header('User-Agent'))
 print(request.get_header('Header1'))
 print(reponse.code)
 html = reponse.read()
-with open('baidu.html','w') as f:
+with open('baidu.html', 'w') as f:
     f.write(html)
 
 print('Done!')
