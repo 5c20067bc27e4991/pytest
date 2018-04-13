2554b76cb6c96a9435f1296b2d6b97bfafa11431
lgl-test <test@liuguanglin.com>
2018-04-13 18:09:16 +0800

    https

--- src/spider1/base.py
+++ src/spider1/base.py
@@ -4,7 +4,7 @@
 import urllib
 import urllib2
 
-baseurl = 'http://www.baidu.com/'
+baseurl = 'https://www.baidu.com/'
 UA = {
     'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.888'}
 
