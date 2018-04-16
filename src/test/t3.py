25b988e5fb55a147d7eda05580ce779b954a1ed0
lgl-test <test@liuguanglin.com>
2018-04-16 18:34:30 +0800

    jb

--- src/test/t3.py
+++ src/test/t3.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import json
+import random
+import os, shutil
+import time
+
+curr_path = os.getcwd()
+
+# os.chdir(r'H:\\')
+os.chdir(r'C:\Users\guanglin\Desktop\t')
+ori_flist = os.listdir('.')
+print(ori_flist)
+while True:
+    curr_time = round(time.time() * 1000)
+    ran_file = random.choice(ori_flist)
+    bname = os.path.basename(ran_file)
+    fname, fsuff = os.path.splitext(bname)
+    dst_name = fname + '_' + str(curr_time) + fsuff
+    try:
+        shutil.copy(ran_file, os.path.join('H:\\', dst_name))
+    except BaseException:
+        print('Copy completed.')
+        break
+    #
+    # shutil.copy(ran_file, dst_name)
+    # print(dst_name)
