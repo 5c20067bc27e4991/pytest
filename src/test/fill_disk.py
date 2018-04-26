149af8ed8bb95e89fa050a3dab8f9a914153ee37
github-test <test@liuguanglin.com>
2018-04-26 23:51:54 +0800

    fill disk

--- src/test/fill_disk.py
+++ src/test/fill_disk.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import random
-import os, shutil
+import os
+import shutil
+import uuid
 import time
 
 curr_path = os.getcwd()
 
-# os.chdir(r'H:\\')
-os.chdir(r'C:\Users\guanglin\Desktop\t')
+os.chdir(r'C:\Users\administrator\Desktop\t')
 ori_flist = os.listdir('.')
 print(ori_flist)
 while True:
-    curr_time = round(time.time() * 1000)
     ran_file = random.choice(ori_flist)
     bname = os.path.basename(ran_file)
     fname, fsuff = os.path.splitext(bname)
-    dst_name = fname + '_' + str(curr_time) + fsuff
+    # curr_time = round(time.time() * 1000)
+    # dst_name = fname + '_' + str(curr_time) + fsuff
+
+    dst_name = fname + '_' + str(uuid.uuid1()) + fsuff
     try:
         shutil.copy(ran_file, os.path.join('H:\\', dst_name))
+        print(dst_name + ' copied.')
     except BaseException:
         print('Copy completed.')
         break
