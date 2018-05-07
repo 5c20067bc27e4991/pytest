b978178d04c9e387e352eb961d4eb6b651fed965
lgl-test <test@liuguanglin.com>
2018-05-07 14:09:44 +0800

    xx

--- src/test/t2.py
+++ src/test/t2.py
@@ -19,7 +19,8 @@ from collections import Counter
 l = [1, 1, 2, 2, 2, 3, 4, 4, 5]
 print(dict(Counter(l)))
 
-d = {'k1': '1', 'k10': '10', 'k2': '2', 'k4': 'v2', 'k5': 'v5', 'k3': 'v1', 'k6': 'v2'}
+d = {'k1': '20', 'k10': '10', 'k2': '2', 'k4': 'v2', 'k5': 'v5', 'k3': 'v1', 'k6': 'v2'}
 print(sorted(d))
-# for i in sorted(d.items(), key=lambda dic: dic[1]):
-#     print(i[0], i[1])
+from operator import itemgetter
+
+print(sorted(d.items(), key=lambda x: itemgetter(1)(x)))
