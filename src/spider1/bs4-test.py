abdab8f26cab9f36df285525d6079c2492905aa4
lgl-test <test@liuguanglin.com>
2018-05-21 18:49:53 +0800

    bs4 go on

--- src/spider1/bs4-test.py
+++ src/spider1/bs4-test.py
@@ -16,18 +16,18 @@ sp = BeautifulSoup(html_init, 'html5lib')
 # print(GF().f_lineno, sp.prettify())
 # print(sp.get_text())
 # print(sp.getText())
-print(GF().f_lineno, sp.title)
+print(GF().f_lineno, sp.title.encode('latin-1'))
 print(GF().f_lineno, sp.title.name)
 print(GF().f_lineno, sp.div.attrs)
 print(GF().f_lineno, sp.option.string)
 print(GF().f_lineno, type(sp), ' ---- ', type(sp.div), ' ---- ', type(sp.option.string), ' ---- ', type(sp.a.string))
-print(GF().f_lineno, sp.option.contents)
-print(GF().f_lineno, sp.title.parent.name)
+print(GF().f_lineno, sp.head.contents)
+print(GF().f_lineno, sp.title.parent)
 [print(GF().f_lineno, p.name) for p in sp.title.parents]
 print(GF().f_lineno, sp.option['value'])
 print(GF().f_lineno, sp.option.get('value'))
-# [print(child.name) for child in sp.body.children if child.name]
-# [print(child.name) for child in sp.body.descendants if child.name]
+# [print(child.string) for child in sp.body.children if child.name]
+# [print(child) for child in sp.body.descendants if child.name]
 # [print(child) for child in sp.body.strings]
 # [print(child) for child in sp.div.stripped_strings]  # 除去空白
 print(GF().f_lineno, sp.find('div'))
@@ -35,4 +35,5 @@ print(GF().f_lineno, sp('div'))
 print(GF().f_lineno, sp.find_all('div'))
 print(GF().f_lineno, sp.find_all(attrs={"class": "c2"}))
 print(GF().f_lineno, sp.find_all("div", "c1"))
-print(GF().f_lineno, sp.div['class'])
+print(GF().f_lineno, sp.p.next_sibling)
+print(sp.body.child)
