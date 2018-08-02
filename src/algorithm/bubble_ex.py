e71df73bb31489a2b4ff289f90e4fa5571a5b7ca
github-test <test@liuguanglin.com>
2018-08-03 00:45:32 +0800

    bubble optimization

--- src/algorithm/bubble_ex.py
+++ src/algorithm/bubble_ex.py
@@ -1,22 +1,21 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-Created on 2017年3月24日
-@author: guanglin
-'''
-import random, time
+
+import random
+import time
 
 lst = list(range(10))
 random.shuffle(lst)
-# t1 = time.time()
 print(lst)
+# t1 = time.time()
 for i in range(len(lst) - 1):
+    exchange = False
     for j in range(len(lst) - 1 - i):
         if lst[j] > lst[j + 1]:
-            # t=lst[j]
-            # lst[j]=lst[j+1]
-            # lst[j+1]=t
             lst[j], lst[j + 1] = lst[j + 1], lst[j]
-print(lst)
+            exchange = True
+    if not exchange:
+        break
 # t2 = time.time()
 # print(t2 - t1)
-# 时间复杂度O(n^2)
+print(lst)
